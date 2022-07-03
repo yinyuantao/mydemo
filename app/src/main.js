41688bd5c377bcbb0bfebaba56269be301e3dbe0
yyt <429333984@qq.com>
2022-07-03 14:42:25 +0800

    旭旭我更新了

--- app/src/main.js
+++ app/src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
-import store from './store'
+import store from '@/store'
 import TypeNav from '@/components/typeNav'
 Vue.config.productionTip = false
 Vue.component(TypeNav.name,TypeNav)
 
 new Vue({
   render: h => h(App),
-  router,
-  store,
+  router,//注册路由
+  store,//注册仓库
 }).$mount('#app')

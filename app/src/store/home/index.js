540535edc850d5ddbaceda30d3ebe4e0eb59aba3
yyt <429333984@qq.com>
2022-07-06 15:41:10 +0800

    旭旭我更新了

--- app/src/store/home/index.js
+++ app/src/store/home/index.js
@@ -1,6 +1,22 @@
-const state={};
-const mutations={};
-const actions={};
+import { reqCateCoryList } from "@/api";
+
+const state={ 
+    categoryList:[],
+};
+const mutations={ 
+    CATEGORYLIST(state,categoryList){ 
+        state.categoryList = categoryList
+    }
+};
+const actions={ 
+    async categoryList({ commit}){ 
+        let result = await reqCateCoryList()
+        console.log(result);
+        if(result.code == 200){ 
+            commit('CATEGORYLIST', result.data)
+        }
+    }
+};
 const getters={};
 
 export default{ 

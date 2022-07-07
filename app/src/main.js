4e3a5bbec69d3702f18ec8216ebfa3e4d6f21d80
yyt <429333984@qq.com>
2022-07-07 14:13:21 +0800

    我更新了

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
 import TypeNav from '@/components/typeNav'
+import '@/mock/mockServer'
 Vue.config.productionTip = false
 Vue.component(TypeNav.name,TypeNav)
 
--- app/src/mock/mockServer.js
+++ app/src/mock/mockServer.js
@@ -3,5 +3,11 @@ import Mock from 'mockjs'
 import banner from './banner.json'
 import floor from './floor.json'
 
-Mock.mock('/mock/banner',{code:200,data:banner})
-Mock.mock()
+Mock.mock('/mock/banner',{ 
+    code:200,
+    data:banner
+})
+Mock.mock('/mock/floors',{ 
+    code:200,
+    data:floor
+})

3084edbcf702859f03f8186977c600aea095bddc
yyt <429333984@qq.com>
2022-07-07 16:18:53 +0800

    更新了哦 荣荣

--- app/src/main.js
+++ app/src/main.js
@@ -4,6 +4,7 @@ import router from '@/router'
 import store from '@/store'
 import TypeNav from '@/components/typeNav'
 import '@/mock/mockServer'
+import 'swiper/css/swiper.css'
 Vue.config.productionTip = false
 Vue.component(TypeNav.name,TypeNav)
 

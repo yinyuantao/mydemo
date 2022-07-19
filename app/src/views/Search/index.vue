9ca7985c56d64d5ef907438a0d2418f039e50441
yyt <429333984@qq.com>
2022-07-19 17:56:41 +0800

    更新了搜索

--- app/src/views/Search/index.vue
+++ app/src/views/Search/index.vue
@@ -71,11 +71,11 @@
               <li class="yui3-u-1-5" v-for="(good,index) in goodsList" :key="good.id">
                 <div class="list-wrap">
                   <div class="p-img">
-                    <a href="item.html" target="_blank"><img :src="good.defaultImg" /></a>
+                   <router-link :to="`/detail/${good.id}`"></router-link><img :src="good.defaultImg" /></a>
                   </div>
                   <div class="price">
                     <strong>
-                      <em>¥ &nbsp</em>
+                      <em>¥ &nbsp;</em>
                       <i>{{good.price}}</i>
                     </strong>
                   </div>

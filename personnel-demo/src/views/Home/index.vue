ea55263468ad418535ba354dd9b89c278a6b193d
yyt <429333984@qq.com>
2022-07-20 17:56:02 +0800

    更新了首页

---
+++ personnel-demo/src/views/Home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- 左边菜单 -->
+    <Top/>
+    <Main/>
+    <Left /> 
+  </div>
+</template>
+
+<script>
+import Left from './Left/index.vue'
+import Top from './top/top.vue'
+import Main from './main/main.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Left,
+        Top,
+        Main
+    },
+}
+</script>
+
+<style>
+
+</style>

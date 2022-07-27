2e34682911f45e957f5de574d423230b99c0042a
yyt <429333984@qq.com>
2022-07-27 17:57:37 +0800

    最后下班前更新

---
+++ personnel-demo/src/views/transferManagement/transferReview/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="left">
+    <div>
+      您正在做的业务是：<span>人力资源</span>--<span>人力资源档案管理</span>--<span
+        >人力资源档案登记</span
+      >
+    </div>
+        <el-button type="warning">查询</el-button>
+
+    <table border="1px">
+      <tr>
+        <td>请选择员工所在一级机构</td>
+        <td></td>
+        <td>请选择员工所在的二级机构</td>
+        <td></td>
+        <td>请选择员工所在的三级机构</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>请输入建档时间</td>
+        <td></td>
+        <td>至</td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "interviewRegistration",
+};
+</script>
+
+<style scoped lang="scss">
+.left table {
+  margin-top: 30px;
+  border-collapse: collapse;
+  text-align: center;
+}
+th,
+td {
+  border: 1px solid black;
+  height: 40px;
+  width: 150px;
+  text-align: center;
+}
+</style>

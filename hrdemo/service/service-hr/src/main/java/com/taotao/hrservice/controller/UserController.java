c08603c8bddd2a5782e62c237e3c1bfcac493ad9
yinyuantao <429333984@qq.com>
2022-08-18 20:25:49 +0800

    用户注册

--- hrdemo/service/service-hr/src/main/java/com/taotao/hrservice/controller/UserController.java
+++ hrdemo/service/service-hr/src/main/java/com/taotao/hrservice/controller/UserController.java
@@ -2,14 +2,13 @@ package com.taotao.hrservice.controller;
 
 
 import com.taotao.commonutils.R;
+import com.taotao.hrservice.entity.JobType;
 import com.taotao.hrservice.entity.User;
 import com.taotao.hrservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -33,6 +32,23 @@ public class UserController {
         }else {
             return R.error();
         }
+    }
+    //显示所有用户
+    @GetMapping("showAllUser")
+    public R showAllUser(){
+        List<User> users = userService.list(null);
+        if (users == null){
+            return R.error();
+        }else {
+            return R.ok().data("users",users);
+        }
+
+    }
+
+    @GetMapping("/getJobById/{id}")
+    public R getJobById(@PathVariable Integer id){
+        User list = userService.getById(id);
+        return R.ok().data("list",list);
     }
 }
 
--- hrdemo/service/service-hr/src/main/java/com/taotao/hrservice/entity/User.java
+++ hrdemo/service/service-hr/src/main/java/com/taotao/hrservice/entity/User.java
@@ -1,11 +1,9 @@
 package com.taotao.hrservice.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
-
+import com.baomidou.mybatisplus.annotation.IdType;
 import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -29,8 +27,8 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "id")
-    @TableId(value = "id", type = IdType.AUTO)
-    private Integer id;
+    @TableId(value = "user_id", type = IdType.AUTO)
+    private Integer userId;
 
     @ApiModelProperty(value = "用户编号")
     private Integer uId;
@@ -44,18 +42,13 @@ public class User implements Serializable {
     @ApiModelProperty(value = "用户密码")
     private String uPassword;
 
-
-    @ApiModelProperty(value = "删除结果")
-    @TableLogic(value = "1", delval = "0")
-    @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "逻辑删除")
     private Integer isDelete;
 
     @ApiModelProperty(value = "创建时间")
-    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
     @ApiModelProperty(value = "更新时间")
-    @TableField(fill = FieldFill.UPDATE)
     private Date updateTime;
 
 
